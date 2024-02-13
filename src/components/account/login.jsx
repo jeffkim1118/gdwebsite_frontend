3f37195ccf2a806dc43ceadc74c459a4a4e3afc0
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2024-02-12 21:06:02 -0500

    repositioned the login form and register form

--- src/components/account/login.jsx
+++ src/components/account/login.jsx
@@ -41,7 +41,6 @@ export default function Login() {
                 <button onClick={(e) => handleSubmit(e)}>Login</button>
                 <Link to='/recover'>Forgot your password?</Link>
             </form>
-            <img className="login-page-image" src="https://images.unsplash.com/photo-1580234811497-9df7fd2f357e?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="game-photo"></img>
         </div>
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import SignOut from "./account/signout";
 
-export default function Navbar(){
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 fixed">
+      <a className="btn btn-ghost text-xl" href="/">GD</a>
+      <div>
+        <a className="btn btn-ghost" href="/">First</a>
+        <a className="btn btn-ghost" href="/login">Login</a>
+        <a className="btn btn-ghost" href="/register">Register</a>
+      </div>
 
-    return(
-        <div>
-            <nav className="navigation-bar">
+      {/* <nav className="navigation-bar">
                 <a href="/">GD</a>
                 <a href="#">Deals</a>
                 <a href="#">News</a>
@@ -14,7 +20,7 @@ export default function Navbar(){
                 <a href="/login">Login</a>
                 <a href="/register">Register</a>
                 <SignOut/>
-            </nav>
-        </div>
-    )
+            </nav> */}
+    </div>
+  );
 }
